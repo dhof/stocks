16db40bb79c0679155d4f1431f5760af166f717a
dhof <devon_hof@yahoo.com>
2017-11-11 17:02:19 -0700

    added static data

---
+++ src/data.js
@@ -0,0 +1,301 @@
+0
+:
+"MSFT"
+1
+:
+"BRKA"
+2
+:
+"AAPL"
+3
+:
+"INFO"
+4
+:
+"F"
+5
+:
+"PLT"
+6
+:
+"BIG"
+7
+:
+"TWX"
+8
+:
+"AME"
+9
+:
+"JWN"
+10
+:
+"CVS"
+11
+:
+"MS"
+12
+:
+"MET"
+13
+:
+"IVD"
+14
+:
+"RND"
+
+MSFT {
+  Change: 0.519999999999996,
+  ChangePercent: 0.724637681159415,
+  ChangePercentYTD: 16.3179916317992,
+  ChangeYTD: 62.14,
+  High: 72.89,
+  LastPrice: 72.28,
+  Low: 71.81,
+  MarketCap: 558038824200,
+  MSDate: 42891,
+  Name: "Microsoft Corp",
+  Open: 71.97,
+  Status: "SUCCESS",
+  Symbol: "MSFT",
+  Timestamp: "Mon Jun 5 00:00:00 UTC-04:00 2017",
+  Volume: 33316760
+}
+
+BRKA {
+  Change: -550,
+  ChangePercent: -0.22,
+  ChangePercentYTD: 2.18293387295644,
+  ChangeYTD: 244121,
+  High: 250012,
+  LastPrice: 249450,
+  Low: 248650,
+  MarketCap: 191827050000,
+  MSDate: 42892.6520023148,
+  Name: "Berkshire Hathaway Inc",
+  Open: 249200.01,
+  Status: "SUCCESS",
+  Symbol: "BRK.A",
+  Timestamp: "Tue Jun 6 15:38:53 UTC-04:00 2017",
+  Volume: 297
+}
+
+AAPL {
+  Change: -1.51999999999998,
+  ChangePercent: -0.977806368607258,
+  ChangePercentYTD: 32.9045069936108,
+  ChangeYTD: 115.82,
+  High: 154.45,
+  LastPrice: 153.93,
+  Low: 153.46,
+  MarketCap: 802566391200,
+  MSDate: 42891,
+  Name: "Apple Inc",
+  Open: 154.34,
+  Status: "SUCCESS",
+  Symbol: "AAPL",
+  Timestamp: "Mon Jun 5 00:00:00 UTC-04:00 2017",
+  Volume: 25331662
+}
+
+INFO {
+  Change: -0.260000000000005,
+  ChangePercent: -0.552486187845315,
+  ChangePercentYTD: 32.1660547867834,
+  ChangeYTD: 35.41,
+  High: 47.2,
+  LastPrice: 46.8,
+  Low: 46.71,
+  MarketCap: 19041750000,
+  MSDate: 42891,
+  Name: "IHS Markit Ltd",
+  Open: 47.03,
+  Status: "SUCCESS",
+  Symbol: "INFO",
+  Timestamp: "Mon Jun 5 00:00:00 UTC-04:00 2017",
+  Volume: 4082775
+}
+
+F {
+  Change: -0.109999999999999,
+  ChangePercent: -0.977777777777773,
+  ChangePercentYTD: -8.16158285243199,
+  ChangeYTD: 12.13,
+  High: 11.2,
+  LastPrice: 11.14,
+  Low: 11.05,
+  MarketCap: 43570032760,
+  MSDate: 42892.6552777808,
+  Name: "Ford Motor Co",
+  Open: 11.19,
+  Status: "SUCCESS",
+  Symbol: "F",
+  Timestamp: "Tue Jun 6 15:43:36 UTC-04:00 2017",
+  Volume: 40054809
+}
+
+PLT {
+  Change: -0.18,
+  ChangePercent: -0.323682790864952,
+  ChangePercentYTD: 1.22352081811542,
+  ChangeYTD: 54.76,
+  High: 55.94,
+  LastPrice: 55.43,
+  Low: 55.02,
+  MarketCap: 1853412910,
+  MSDate: 42892.6617824084,
+  Name: "Plantronics Inc",
+  Open: 55.44,
+  Status: "SUCCESS",
+  Symbol: "PLT",
+  Timestamp: "Tue Jun 6 15:52:58 UTC-04:00 2017",
+  Volume: 94192
+}
+
+BIG {
+  Change: -1.36,
+  ChangePercent: -2.73972602739726,
+  ChangePercentYTD: -3.84385580561641,
+  ChangeYTD: 50.21,
+  High: 49.17,
+  LastPrice: 48.28,
+  Low: 47.99,
+  MarketCap: 2162268080,
+  MSDate: 42892.6620949074,
+  Name: "Big Lots Inc",
+  Open: 49.17,
+  Status: "SUCCESS",
+  Symbol: "BIG",
+  Timestamp: "Tue Jun 6 15:53:25 UTC-04:00 2017",
+  Volume: 1253948
+}
+
+TWX {
+  Change: -0.240000000000009,
+  ChangePercent: -0.240722166499508,
+  ChangePercentYTD: 3.03532580544908,
+  ChangeYTD: 96.53,
+  High: 99.82,
+  LastPrice: 99.46,
+  Low: 99.44,
+  MarketCap: 77135009480,
+  MSDate: 42892.6622916667,
+  Name: "Time Warner Inc",
+  Open: 99.65,
+  Status: "SUCCESS",
+  Symbol: "TWX",
+  Timestamp: "Tue Jun 6 15:53:42 UTC-04:00 2017",
+  Volume: 2953535
+}
+
+AME {
+  Change: -0.269999999999996,
+  ChangePercent: -0.436893203883489,
+  ChangePercentYTD: 26.6049382716049,
+  ChangeYTD: 48.6,
+  High: 61.76,
+  LastPrice: 61.53,
+  Low: 61.25,
+  MarketCap: 14158545240,
+  MSDate: 42892.6622569544,
+  Name: "Ametek Inc",
+  Open: 61.38,
+  Status: "SUCCESS",
+  Symbol: "AME",
+  Timestamp: "Tue Jun 6 15:53:39 UTC-04:00 2017",
+  Volume: 601580
+}
+
+JWN {
+  Change: -1.565,
+  ChangePercent: -3.75750300120047,
+  ChangePercentYTD: -16.367619445024,
+  ChangeYTD: 47.93,
+  High: 41.66,
+  LastPrice: 40.085,
+  Low: 39.715,
+  MarketCap: 6656194420,
+  MSDate: 42892.6625810225,
+  Name: "Nordstrom Inc",
+  Open: 41.51,
+  Status: "SUCCESS",
+  Symbol: "JWN",
+  Timestamp: "Tue Jun 6 15:54:07 UTC-04:00 2017",
+  Volume: 4133041
+}
+
+CVS {
+  Change: -0.984999999999999,
+  ChangePercent: -1.25015864957482,
+  ChangePercentYTD: -1.40032948929159,
+  ChangeYTD: 78.91,
+  High: 78.52,
+  LastPrice: 77.805,
+  Low: 77.64,
+  MarketCap: 79267344975,
+  MSDate: 42892.6627199094,
+  Name: "CVS Health Corp",
+  Open: 78.41,
+  Status: "SUCCESS",
+  Symbol: "CVS",
+  Timestamp: "Tue Jun 6 15:54:19 UTC-04:00 2017",
+  Volume: 3812490
+}
+
+MS {
+  Change: 0.104999999999997,
+  ChangePercent: 0.244812310561895,
+  ChangePercentYTD: 1.76331360946745,
+  ChangeYTD: 42.25,
+  High: 43.07,
+  LastPrice: 42.995,
+  Low: 42.21,
+  MarketCap: 79531377090,
+  MSDate: 42892.6628587963,
+  Name: "Morgan Stanley",
+  Open: 42.44,
+  Status: "SUCCESS",
+  Symbol: "MS",
+  Timestamp: "Tue Jun 6 15:54:31 UTC-04:00 2017",
+  Volume: 5471583
+}
+
+MET {
+  Change: -0.705000000000005,
+  ChangePercent: -1.37883825542735,
+  ChangePercentYTD: -6.42976433475599,
+  ChangeYTD: 53.89,
+  High: 50.83,
+  LastPrice: 50.425,
+  Low: 50.13,
+  MarketCap: 54253921525,
+  MSDate: 42892.6630324074,
+  Name: "Metlife Inc",
+  Open: 50.7,
+  Status: "SUCCESS",
+  Symbol: "MET",
+  Timestamp: "Tue Jun 6 15:54:46 UTC-04:00 2017",
+  Volume: 3751545
+}
+
+IVD {
+  Message: "No symbol matches found for RDM. Try another symbol such as MSFT or AAPL, or use the Lookup API.",
+  Status: "FAILED"
+}
+
+RND {
+  Change: -1.089709777474873,
+  ChangePercent: 8.228990072005814,
+  ChangePercentYTD: -9.498026142686129,
+  ChangeYTD: 42.08639157713138,
+  High: 46.755113562413484,
+  LastPrice: 24.861486007956387,
+  Low: 12.074802761077864,
+  MarketCap: 5107215394203080,
+  Name: "Random Company Date Example",
+  Open: 523.5399976908225,
+  Status: "SUCCESS",
+  Symbol: "RND",
+  Timestamp: "Sat Nov 11 2017 17:01:08 GMT-0700 (Mountain Standard Time)",
+  Volume: 4087557001583609
+}
