4125418df66c0662ec93a954e0459738e5ecd74c
dhof <devon_hof@yahoo.com>
2018-01-17 02:39:54 -0700

    update firebase config to use env vars

--- src/marketApiCalls.js
+++ src/marketApiCalls.js
@@ -1,6 +1,14 @@
 import axios from 'axios'
 import * as firebase from 'firebase'
-import { firebaseConfig } from './config/Config.js'
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_DOMAIN,
+	databaseURL: process.env.REACT_APP_DB_URL,
+	projectId: process.env.REACT_APP_PROJ_ID,
+	storageBucket: process.env.REACT_APP_BUCKET,
+	messagingSenderId: process.env.REACT_APP_SENDER_ID
+}
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
